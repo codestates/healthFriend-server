4df0ea1e4d82184865ae4a27f94c9baa78e434b9
LeeSuHo <angelxtry@gmail.com>
2019-12-20 16:04:27 +0900

    graphql 설치
    다음의 package를 설치
    yarn add graphql apollo-server-express express
    index.ts 파일을 수정하여 간단한 테스트 수행
    nodemon 설치, package.json의 script 수정함

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
 import 'reflect-metadata';
-import { createConnection, getConnectionOptions } from 'typeorm';
-import { User } from './entity/User';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-const testDbCommection = async () => {
-  const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
-  createConnection({ ...connectionOptions, name: 'default' })
-    .then(async (connection) => {
-      console.log('Inserting a new user into the database...');
-      const user = new User();
-      user.firstName = 'Timber';
-      user.lastName = 'Saw';
-      user.age = 25;
-      await connection.manager.save(user);
-      console.log(`Saved a new user with id: ${user.id}`);
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+  type Mutation {
+    helloUser(name: String): String!
+  }
+`;
 
-      console.log('Loading users from the database...');
-      const users = await connection.manager.find(User);
-      console.log('Loaded users: ', users);
-
-      console.log(
-        'Here you can setup and run express/koa/any other framework.',
-      );
-    })
-    .catch((error) => console.log(error));
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World',
+  },
+  Mutation: {
+    helloUser: (_: any, { name }: any) => `Hello ${name || 'World'}`,
+  },
 };
 
-testDbCommection();
+const server = new ApolloServer({ typeDefs, resolvers });
+const app = express();
+
+server.applyMiddleware({ app });
+app.listen({ port: 4000 }, () => {
+  console.log(`Server running at http://localhost:4000${server.graphqlPath}`);
+});
