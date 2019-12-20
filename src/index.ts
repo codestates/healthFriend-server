8390e68678d123354725010c513374bdf98f6fb9
LeeSuHo <angelxtry@gmail.com>
2019-12-21 00:45:25 +0900

    google login 코드를 index.ts 파일에 적용
    passport 등록 및 호출.
    google login을 위한 routing 파일 등록.
    connectDB가 비동기 함수인 점을 감안하여 async/await로 실행.

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,50 @@
 import 'reflect-metadata';
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
+import passport from 'passport';
 
 import connectDB from './utils/connectDB';
+import authRouter from './auth/routes';
+import passportConfig from './auth';
 
-const typeDefs = gql`
-  type Query {
-    hello: String!
-  }
-  type Mutation {
-    helloUser(name: String): String!
-  }
-`;
+const startServer = async () => {
+  try {
+    const typeDefs = gql`
+      type Query {
+        hello: String!
+      }
+      type Mutation {
+        helloUser(name: String): String!
+      }
+    `;
 
-const resolvers = {
-  Query: {
-    hello: () => 'Hello World',
-  },
-  Mutation: {
-    helloUser: (_: any, { name }: any) => `Hello ${name || 'World'}`,
-  },
+    const resolvers = {
+      Query: {
+        hello: () => 'Hello World',
+      },
+      Mutation: {
+        helloUser: (_: any, { name }: any) => `Hello ${name || 'World'}`,
+      },
+    };
+
+    await connectDB();
+    passportConfig();
+
+    const server = new ApolloServer({ typeDefs, resolvers });
+    const app = express();
+
+    app.use(passport.initialize());
+    app.use('/auth', authRouter);
+
+    server.applyMiddleware({ app });
+    app.listen({ port: 4000 }, () => {
+      console.log(
+        `Server running at http://localhost:4000${server.graphqlPath}`,
+      );
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-connectDB();
-const server = new ApolloServer({ typeDefs, resolvers });
-const app = express();
-
-server.applyMiddleware({ app });
-app.listen({ port: 4000 }, () => {
-  console.log(`Server running at http://localhost:4000${server.graphqlPath}`);
-});
+startServer();
