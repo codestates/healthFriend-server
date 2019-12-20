52a724634166b8a13c90182549a4f5dfe20538d3
LeeSuHo <angelxtry@gmail.com>
2019-12-20 12:20:57 +0900

    typescript 설정
    tsconfig 수정
    eslint 설정
    - .eslintrc.js 추가
    - .prettire 추가

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'airbnb-base'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      { ts: 'never', js: 'never' }
+    ],
+    'class-methods-use-this': [0],
+    'import/prefer-default-export': [0],
+    'no-console': 0,
+    'import/no-extraneous-dependencies': 0,
+    'max-len': ["error", { "code": 80 }],
+    'no-param-reassign': ["error", {
+      props: true,
+      ignorePropertyModificationsFor: ['req', 'request', 'session']
+    }],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': [2, { args: 'none' }]
+      }
+    }
+  ]
+};
