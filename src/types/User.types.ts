aba56f2ea5147188ffbe58ce1856b342b8b2a7a0
LeeSuHo <angelxtry@gmail.com>
2019-12-24 16:00:35 +0900

    User 관련 interface 추가
    SimpleUserInfo, DetailedUserInfo 추가

---
+++ src/types/User.types.ts
@@ -0,0 +1,14 @@
+import { OpenImageChoice, LevelOf3Dae } from '../entity/User';
+
+export interface SimpleUserInfo {
+  id: string,
+  email: string,
+  nickname: string,
+}
+
+export interface DetailedUserInfo {
+  nickname: string,
+  openImageChoice: OpenImageChoice,
+  levelOf3Dae: LevelOf3Dae,
+  messageToFriend: string,
+}
