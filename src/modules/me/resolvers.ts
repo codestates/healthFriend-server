c2fc71038b43a57d9df34122f932e62144e81e67
LeeSuHo <angelxtry@gmail.com>
2019-12-24 16:24:07 +0900

    사용자 정보 변경 기능 추가

--- src/modules/me/resolvers.ts
+++ src/modules/me/resolvers.ts
@@ -1,15 +1,37 @@
+import { DetailedUserInfo } from '../../types/User.types';
 import { getUserRepository } from '../../utils/connectDB';
 import { createMiddleware } from '../../utils/createMiddleware';
 import middleware from './middleware';
 
 const resolvers = {
+  OpenImageChoice: {
+    OPEN: 'open',
+    FRIEND: 'friend',
+    CLOSE: 'close',
+  },
+  LevelOf3Dae: {
+    L1: '1: 0 ~ 99',
+    L2: '2: 100 ~ 199',
+    L3: '3: 200 ~ 299',
+    L4: '4: 300 ~ 399',
+    L5: '5: 400 ~ 499',
+  },
   Query: {
-    hello: () => 'Hello World',
     me: createMiddleware(middleware, (_: any, __: any, context: any) =>
       getUserRepository().findByUserId(context.userInfo.id)),
   },
   Mutation: {
-    helloUser: (_: any, { name }: any) => `Hello ${name || 'World'}`,
+    me: createMiddleware(
+      middleware,
+      async (_: any, args: DetailedUserInfo, context: any) => {
+        const user = await getUserRepository().updateUserInfo(
+          context.userInfo.id,
+          args,
+        );
+        console.log(user);
+        return user;
+      },
+    ),
   },
 };
 
--- src/modules/me/typeDefs.ts
+++ src/modules/me/typeDefs.ts
@@ -1,6 +1,25 @@
 import { gql } from 'apollo-server-express';
 
 const typeDefs = gql`
+  enum OpenImageChoice {
+    OPEN
+    FRIEND
+    CLOSE
+  }
+
+  enum LevelOf3Dae {
+    "L1 = '1: 0 ~ 99'"
+    L1
+    "L2 = '2: 100 ~ 199'"
+    L2
+    "L3 = '3: 200 ~ 299'"
+    L3
+    "L4 = '4: 300 ~ 399'"
+    L4
+    "L5 = '5: 400 ~ 499'"
+    L5
+  }
+
   type User {
     id: ID!
     email: String!
@@ -11,12 +30,23 @@ const typeDefs = gql`
   }
 
   type Query {
-    hello: String!
     me: User
   }
 
   type Mutation {
-    helloUser(name: String): String!
+    me(
+      nickname: String!
+      openImageChoice: OpenImageChoice!
+      """
+      L1 = '1: 0 ~ 99'\n
+      L2 = '2: 100 ~ 199'\n
+      L3 = '3: 200 ~ 299'\n
+      L4 = '4: 300 ~ 399'\n
+      L5 = '5: 400 ~ 499'
+      """
+      levelOf3Dae: LevelOf3Dae!
+      messageToFriend: String
+    ): User!
   }
 `;
 
