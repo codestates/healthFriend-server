862d5860b3aff6163518da681bb0c3c7e42df7fb
LeeSuHo <angelxtry@gmail.com>
2019-12-30 17:54:46 +0900

    typeDefs 파일을 하나로 통합
    production 상태로 실행할 때 Query를 찾을 수 없다고 에러가 발생함.
    정확한 원인은 미확인.
    typeDefs 파일을 하나로 통합하여 임시로 처리함.

--- src/schema.ts
+++ src/schema.ts
@@ -1,18 +1,20 @@
 import {
   fileLoader,
   mergeResolvers,
-  mergeTypes,
+  // mergeTypes,
 } from 'merge-graphql-schemas';
 import path from 'path';
 import { makeExecutableSchema } from 'graphql-tools';
 import { GraphQLSchema } from 'graphql';
 
-const allTypes = fileLoader(path.join(__dirname, './modules/**/typeDefs.ts'));
+import typeDefs from './modules/shared/typeDefs';
+
+// const allTypes = fileLoader(path.join(__dirname, './modules/**/typeDefs.ts'));
 const allResolvers = fileLoader(
   path.join(__dirname, './modules/**/resolvers.ts'),
 );
 const schema: GraphQLSchema = makeExecutableSchema({
-  typeDefs: mergeTypes(allTypes),
+  typeDefs,
   resolvers: mergeResolvers(allResolvers),
 });
 
