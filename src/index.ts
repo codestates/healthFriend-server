e096815d23d2c992f88e768635fa36698144aafc
LeeSuHo <angelxtry@gmail.com>
2019-12-30 16:09:43 +0900

    https 적용

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { ApolloServer } from 'apollo-server-express';
 import passport from 'passport';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
+import https from 'https';
+import http from 'http';
 
 import connectDB from './database';
 import authRouter from './auth/routes';
@@ -13,10 +15,17 @@ import { getUserInfoFromToken } from './utils/controllToken';
 
 const startServer = async () => {
   try {
+    const configurations = {
+      production: { ssl: true, port: 443, hostname: 'api.healthfriend.com' },
+      development: { ssl: false, port: 4000, hostname: 'localhost' },
+    };
+    const environment = process.env.NODE_ENV || 'production';
+    const config = (configurations as any)[environment];
+
     await connectDB();
     passportConfig();
 
-    const server = new ApolloServer({
+    const apollo = new ApolloServer({
       schema,
       context: ({ req }: any) => ({
         userInfo: getUserInfoFromToken(req.cookies['access-token']),
@@ -39,12 +48,19 @@ const startServer = async () => {
       origin: true,
     };
 
-    server.applyMiddleware({ app, cors });
-    app.listen({ port: 4000 }, () => {
+    apollo.applyMiddleware({ app, cors });
+
+    const server = config.ssl
+      ? https.createServer(app)
+      : http.createServer(app);
+
+    server.listen({ port: config.port }, () =>
       console.log(
-        `Server running at http://localhost:4000${server.graphqlPath}`,
-      );
-    });
+        'Server ready at',
+        `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}${
+          apollo.graphqlPath
+        }`,
+      ));
   } catch (error) {
     console.error(error);
   }
