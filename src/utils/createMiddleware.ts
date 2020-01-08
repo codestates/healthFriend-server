1ebdf0ac1b435dcb2bce6f11dd12f42ca532908e
LeeSuHo <angelxtry@gmail.com>
2020-01-08 14:55:09 +0900

    인증 방식 변경
    기존 cookie의 access-token을 확인하던 것을
    header.authorization에서 token을 확인하는 로직을 추가
    가능하다면 cookie를 확인하는 로직은 삭제하려고 함
    me schema에 적용

--- src/utils/createMiddleware.ts
+++
@@ -1,6 +0,0 @@
-export const createMiddleware = (
-  middlewareFunc: any,
-  resolverFunc: any,
-) => (parent: any, args: any, context: any, info: any) => middlewareFunc(
-  resolverFunc, parent, args, context, info,
-);
