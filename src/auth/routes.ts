44f47ee735540cbc25906c51b5602573a147c54b
LeeSuHo <angelxtry@gmail.com>
2020-01-11 17:07:41 +0900

    stream chat token 생성 후 cookie로 전달하는 기능 추가

--- src/auth/routes.ts
+++ src/auth/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import passport from 'passport';
+import { StreamChat } from 'stream-chat';
 
 import { TokenUserInfo } from '../types/User.types';
 import { createToken } from '../utils/controllToken';
@@ -15,10 +16,18 @@ router.get(
   '/google/callback',
   passport.authenticate('google', { session: false }),
   (req, res) => {
-    const accessToken = createToken(req.user as TokenUserInfo);
+    const user = req.user as TokenUserInfo;
+    const accessToken = createToken(user as TokenUserInfo);
     if (!accessToken) return res.status(401).send('Login Error.');
 
+    const client = new StreamChat('', '{{ chat_api_secret }}');
+    const streamChatToken = client.createToken(user.id);
+
     res.cookie('access-token', accessToken, {
+      // httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+    });
+    res.cookie('stream-chat-token', streamChatToken, {
       // httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24, // 1 day
     });
