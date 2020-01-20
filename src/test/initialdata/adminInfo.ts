0d02c488183ffc1382a5e4fe669a3b55cca04bb7
LeeSuHo <angelxtry@gmail.com>
2020-01-20 12:19:23 +0900

    admin test data를 저장가능하도록 수정

--- src/test/initialdata/adminInfo.ts
+++ src/test/initialdata/adminInfo.ts
@@ -6,17 +6,20 @@ import {
   Role,
 } from '../../database/entity/User';
 
-const admin = [
-  {
-    role: Role.ADMIN,
-    email: 'admin@hf.club',
-    nickname: 'admin',
-    gender: Gender.MALE,
-    provider: Provider.GOOGLE,
-    snsId: 'abc123',
-    openImageChoice: OpenImageChoice.OPEN,
-    levelOf3Dae: LevelOf3Dae.L1,
-  },
-];
+const adminInfo = {
+  role: Role.ADMIN,
+  email: 'admin@hf.club',
+  nickname: 'admin',
+  gender: Gender.MALE,
+  provider: Provider.GOOGLE,
+  snsId: 'abc123',
+  openImageChoice: OpenImageChoice.OPEN,
+  levelOf3Dae: LevelOf3Dae.L1,
+  motivations: ['WEIGHT_INCREASE'],
+  ableDays: ['MONDAY'],
+  districts: [
+    { nameOfDong: '대치1동', idOfGu: 1, nameOfGu: '강남구' },
+  ],
+};
 
-export default admin;
+export default adminInfo;
