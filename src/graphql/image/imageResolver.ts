85e7e91a6b469ed57e124b10b7b7d93e999db584
LeeSuHo <angelxtry@gmail.com>
2020-01-24 00:21:02 +0900

    deleteProfileImage schema 추가

--- src/graphql/image/imageResolver.ts
+++ src/graphql/image/imageResolver.ts
@@ -8,8 +8,13 @@ interface GraphqlFile {
   encoding: string;
   createReadStream: any;
 }
+
 interface FileArgs {
   file: Promise<GraphqlFile>;
+}
+
+interface FileUrl{
+  url: string;
 }
 
 const imageResolver = {
@@ -22,6 +27,15 @@ const imageResolver = {
         // console.log('Mutation - profileImageUpload: ', me);
         // console.log('Mutation - profileImageUpload: ', file);
         const result = await getImageRepo().saveImages(me, file);
+        return result;
+      },
+    ),
+
+    deleteProfileImage: combineResolvers(
+      isAuthenticated,
+      async (_: any, args: FileUrl, { userInfo }) => {
+        const me = await getUserRepository().validateUserId(userInfo.id);
+        const result = await getImageRepo().deleteImage(me, args.url);
         return result;
       },
     ),
