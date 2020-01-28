5c2aab1ec27e5496f25f4090ef9a15900be6f1e6
LeeSuHo <angelxtry@gmail.com>
2020-01-28 15:40:00 +0900

    불필요한 주석 삭제

--- src/auth/google.ts
+++ src/auth/google.ts
@@ -10,7 +10,6 @@ export default () => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID as string,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        // eslint-disable-next-line no-nested-ternary
         callbackURL: process.env.NODE_ENV === 'production'
           ? 'https://api.healthfriend.club/auth/google/callback'
           : 'http://localhost:4000/auth/google/callback',
